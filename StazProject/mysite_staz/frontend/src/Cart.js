8e93b23a8d138374e62a97638d6ca2bd1124bc6f
Alex-L-K-free <freeworkit@gmail.com>
2025-01-15 16:19:04 +0300

    проект StazProject с DFR+React оптимизирован

--- StazProject/mysite_staz/frontend/src/Cart.js
+++ StazProject/mysite_staz/frontend/src/Cart.js
@@ -1,7 +1,6 @@
 // frontend/src/Cart.js
 // import React, { useState, useEffect } from 'react';
 // import { fetchCart } from './axiosConfig';
-//
 // import React from 'react';
 // import useFetchData from './hooks/useFetchData'; // Импорт хука
 // import { fetchCart } from './axiosConfig'; // Импорт функции для запроса API
@@ -35,80 +34,5 @@ const Cart = ({ token }) => {
 };
 
 export default Cart;
-
-
-// const Cart = ({ token }) => {
-//   // Используем хук useFetchData
-//   const { data: cart, error, loading } = useFetchData(fetchCart, token);
-//
-//   if (loading) return <div>Загрузка...</div>;
-//   if (error) return <div>{error}</div>;
-//
-//   return (
-//     <div>
-//       <h2>Ваша корзина</h2>
-//       <ul>
-//         {cart.length === 0 ? (
-//           <li>Ваша корзина пуста</li>
-//         ) : (
-//           cart.map((item) => (
-//             <li key={item.id}>
-//               {item.name} - {item.price} руб.
-//             </li>
-//           ))
-//         )}
-//       </ul>
-//     </div>
-//   );
-// };
-//
-// export default Cart;
-
-
-
-
-// const Cart = ({ token }) => {
-//   const [cart, setCart] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//
-//   useEffect(() => {
-//     const loadCart = async () => {
-//       setLoading(true);
-//       try {
-//         const response = await fetchCart(token);
-//         setCart(response.data);
-//       } catch (err) {
-//         setError('Ошибка при загрузке корзины');
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//
-//     loadCart();
-//   }, [token]);
-//
-//   if (loading) return <div>Загрузка...</div>;
-//   if (error) return <div>{error}</div>;
-//
-//   return (
-//     <div>
-//       <h2>Ваша корзина</h2>
-//       <ul>
-//         {cart.length === 0 ? (
-//           <li>Ваша корзина пуста</li>
-//         ) : (
-//           cart.map((item) => (
-//             <li key={item.id}>
-//               {item.name} - {item.price} руб.
-//             </li>
-//           ))
-//         )}
-//       </ul>
-//     </div>
-//   );
-// };
-//
-// export default Cart;
 
 
--- StazProject/mysite_staz/frontend/src/HomePage.js
+++ StazProject/mysite_staz/frontend/src/HomePage.js
@@ -1,14 +1,15 @@
 //frontend/src/homepage.js
 import React from 'react';
-import { Box, Typography, Button, Grid } from '@mui/material';
-import { Link } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Box, Typography, Grid } from '@mui/material';
+// import { Box, Typography, Button, Grid } from '@mui/material';
+// import { Link } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
 const HomePage = () => {
   return (
     <>
-      <Header isAuthenticated={false} />
+      {/*<Header isAuthenticated={false} />*/}
       <main>
         <Box sx={{ textAlign: 'center', mt: 4 }}>
           <Typography variant="h4" gutterBottom>
@@ -18,25 +19,25 @@ const HomePage = () => {
             Найдите свою следующую любимую книгу!
           </Typography>
           <Grid container spacing={2} justifyContent="center" sx={{ mt: 2 }}>
-            <Grid item>
-              <Button variant="contained" color="primary" component={Link} to="/cart">
-                Корзина
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button variant="contained" color="secondary" component={Link} to="/register">
-                Регистрация
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button variant="outlined" color="primary" component={Link} to="/login">
-                Вход
-              </Button>
-            </Grid>
+            {/*<Grid item>*/}
+            {/*  <Button variant="contained" color="primary" component={Link} to="/cart">*/}
+            {/*    Корзина*/}
+            {/*  </Button>*/}
+            {/*</Grid>*/}
+            {/*<Grid item>*/}
+            {/*  <Button variant="contained" color="secondary" component={Link} to="/register">*/}
+            {/*    Регистрация*/}
+            {/*  </Button>*/}
+            {/*</Grid>*/}
+            {/*<Grid item>*/}
+            {/*  <Button variant="outlined" color="primary" component={Link} to="/login">*/}
+            {/*    Вход*/}
+            {/*  </Button>*/}
+            {/*</Grid>*/}
           </Grid>
         </Box>
       </main>
-      <Footer />
+      {/*<Footer />*/}
     </>
   );
 };
