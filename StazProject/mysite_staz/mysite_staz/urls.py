42e94c7c3c30612a8e8cdc3e5c3591b033017391
Alex-L-K-free <freeworkit@gmail.com>
2025-01-17 21:21:57 +0300

    проект StazProject DFR+React + добавление товара в корзину промежуточный вариант

--- StazProject/mysite_staz/mysite_staz/urls.py
+++ StazProject/mysite_staz/mysite_staz/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('market.urls')), # Все API-эндпоинты
     path('api/', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
